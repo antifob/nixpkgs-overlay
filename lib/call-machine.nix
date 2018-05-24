b4dfcf40e874d850beb2b7a82bb942bc29a25d8a
Philippe Grégoire <pg@pgregoire.xyz>
2018-05-24 16:51:47 -0400

    callMachine: enable use of our packages

--- lib/call-machine.nix
+++ lib/call-machine.nix
@@ -1,12 +1,23 @@
 path: cfg:
+let
+    #
+    # The top layer can use our overlay if it defines it, but we
+    # cannot since it does not propagate. Install it here.
+    #
+    c = {
+        nixpkgs = {
+            overlays = [(import ./..)] ++ (cfg.nixpkgs.overlays or []);
+        } // (builtins.removeAttrs (cfg.nixpkgs or {}) [ "overlays" ]);
+    } // builtins.removeAttrs cfg [ "nixpkgs" ];
+in
 rec {
     build = eval.config.system.build.toplevel;
 
     config = {
-        imports = [ path cfg ] ++ import ../nixos/modules/module-list.nix;
+        imports = [ path c ] ++ import ../nixos/modules/module-list.nix;
     };
 
     eval = import <nixpkgs/nixos/lib/eval-config.nix> {
-        modules = [ path cfg ] ++ import ../nixos/modules/module-list.nix;
+        modules = [ path c ] ++ import ../nixos/modules/module-list.nix;
     };
 }
